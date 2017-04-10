73b41afe02b03ba42f7aeffe961d0d4e2ff14b30
Jake Davidson <jake.davidson@mines.sdsmt.edu>
2017-04-10 14:31:35 -0600

    added async file loading for ad list

--- background.js
+++ background.js
@@ -1,3 +1,24 @@
+readFile(chrome.runtime.getURL("adlist.txt"));
+//read list of domains to blocks
+function readFile(filepath) {
+  var file = new XMLHttpRequest();
+  file.open("GET", filepath, true);
+  file.onload = function(e) {
+    if (file.readyState === 4) {
+      if(file.status === 200) {
+        parseFile(file.responseText);
+      } else {
+        console.error(xhr.statusText);
+      }
+    }
+  }
+  file.send(null);
+}
+
+function parseFile(file) {
+  console.log(file);
+}
+
 //iterate through all links on page
 //if the link matches an ad, block it
 function test() {
