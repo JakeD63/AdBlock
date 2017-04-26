5d6d023be87a6df6d4185fe457214b92e61b532e
7275639 <jake.davidson@mines.sdsmt.edu>
2017-04-26 13:32:32 -0600

    testing obsver callback routine

--- domParse.js
+++ domParse.js
@@ -1,5 +1,11 @@
 var observer = new MutationObserver(parse);
 
 function parse(mutions) {
-	
+	for(var i = 0; i < mutations.length(); i++) {
+		var addedNodes = mutations[i].addedNodes;
+		for(var j = 0; j < addedNodes.length(); j++) {
+			var curNode = addedNodes[j];
+			console.log(curNode);
+		}
+	}
 }
