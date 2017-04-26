572cf932fac73b5aba70c5652baa4e6f323537aa
7275639 <jake.davidson@mines.sdsmt.edu>
2017-04-26 13:45:23 -0600

    working observer

--- domParse.js
+++ domParse.js
@@ -1,11 +1,16 @@
 var observer = new MutationObserver(parse);
+ observer.observe(document, {subtree:true, childList:true});
+ document.addEventListener('DOMContentLoaded', function() { observer.disconnect() });
 
-function parse(mutions) {
-	for(var i = 0; i < mutations.length(); i++) {
+function parse(mutations) {
+
+	//call scrape routine in case callback returns after partial DOM load
+
+	for(var i = 0; i < mutations.length; i++) {
 		var addedNodes = mutations[i].addedNodes;
-		for(var j = 0; j < addedNodes.length(); j++) {
+		for(var j = 0; j < addedNodes.length; j++) {
 			var curNode = addedNodes[j];
-			console.log(curNode);
+			//process each node
 		}
 	}
 }
