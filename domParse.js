6edc5f953fe266fa198091fe0ff538330f895653
Jake <jake.davidson@mines.sdsmt.edu>
2017-04-26 20:53:58 -0600

    new direction, we can use observers to remove script tags, we will remove other stuff in background.js, observers are near impossible to parse when they return body tags (as far as I could try)

--- domParse.js
+++ domParse.js
@@ -3,16 +3,14 @@ observer.observe(document, {subtree:true, childList:true});
 document.addEventListener('DOMContentLoaded', function() { observer.disconnect() });
 
 function parse(mutations) {
-
 	//call scrape routine in case callback returns after partial DOM load
 	for(var i = 0; i < mutations.length; i++) {
 		var addedNodes = mutations[i].addedNodes;
 		for(var j = 0; j < addedNodes.length; j++) {
 			var n = addedNodes[j];
-			if(n.nodeType == 1) {
-				scrapeNode(n);
-			}	
-
+				if(n.nodeType == 1) {
+					scrapeNode(n);
+				}	
 		}
 	}
 }
@@ -20,54 +18,23 @@ function parse(mutations) {
 function scrapeNode(node) {
 	//switch based on node.tagName
 	switch (node.tagName) {
-		case 'BODY':
-			parseBody(node);
+		case 'SCRIPT':
+			scrapeScript(node);
 			break;
 		case "IFRAME":
 			scrapeIFrame(node);
 			break;
-		default:
-			//console.log(node);
-			break;
 	}
 }
 
-//body nodes can be parsed as iFrames
-//this is not the same as the other iFrames we block
-function parseBody(bodyNode) {
-	var iframe = bodyNode.children;
-	console.log(iframe);
-}
-
-function scrapeLink(node) {
-	for(var i = 0; i < adArray.length; i++) {
-		if(node.href.includes(adArray[i])) {
-			deleteElement(node);
-			break;
-		}
-	}
-}
-
-function scrapeImg(node) {
-
-	for(var i = 0; i < adArray.length; i++) {
-		if(node.src.includes(adArray[i])) {
-			deleteElement(node);
-			break;
-		}
-	}
-}
-
-function scrapeIns(node) {
-	deleteElement(node);
-
-}
 
 function scrapeIFrame(node) {
 	deleteElement(node);
 }
 	
-
+function scrapeScript(node) {
+	deleteElement(node);
+}
 
 
 //removes node element from page
