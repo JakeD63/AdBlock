f9de70734fa6c0337ad5d7fe5446a46f1ee04eb7
Jake <jake.davidson@mines.sdsmt.edu>
2017-04-27 22:10:33 -0600

    documentation

--- state.js
+++ state.js
@@ -1,14 +1,18 @@
+//on page load, check state of disable checkbox
 document.addEventListener('DOMContentLoaded', init, false);
 var disableCheck;
+
 function init() {
+	//add listener for checkbox (no inline js allowed)
 	disableCheck = document.getElementById('disable');
 	disableCheck.addEventListener("click", toggleDisable);
+	//set checkbox initial state from storage
 	chrome.storage.sync.get('disable', function(response) {
 		disableCheck.checked = response.disable;
 	});
 }
 
-//store if the user has disabled the extension
+//update disable state
 function toggleDisable() {
 	if(disableCheck.checked) {
 		chrome.storage.sync.set({'disable': true});
